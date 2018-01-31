5d69c053e6509ee22587367edf9883294bb90b3d
benji <benji.b.ramirez@gmail.com>
2018-01-30 22:12:46 -0800

    creating functions for setting collisions on different tilesets

--- public/eidolon/js/map.js
+++ public/eidolon/js/map.js
@@ -7,10 +7,10 @@
 
 function createMap(state, mapCSVKey, tilesetImageKey) {
 // Collision map
-    var map = state.game.add.tilemap('map', EidolonGlobals.tilesize, EidolonGlobals.tilesize);
+    var map = state.game.add.tilemap(mapCSVKey, EidolonGlobals.tilesize, EidolonGlobals.tilesize);
 
     //  Now add in the tileset
-    map.addTilesetImage('caveTiles');
+    map.addTilesetImage(tilesetImageKey);
     
     //  Create our layer
     state.layer0 = map.createLayer(0);
@@ -18,56 +18,17 @@ function createMap(state, mapCSVKey, tilesetImageKey) {
     //  Resize the world
     state.layer0.resizeWorld();
 
-    /* Set tile callbacks for special tiles */
-    // map.setTileIndexCallback(78, onHit, this);
+    if(tilesetImageKey == 'caveTiles'){
+        //set the collision/callback indices for the caveTiles tileset
+        setCaveMapCollisionTiles(map);
+        console.log("Tried to set the cave map collision tiles");
+    }
 
-
-    /* Set tile collisions for solid impassable tiles */
-
-    // General cave wall edges and fill
-    map.setCollisionBetween(7, 10);
-    map.setCollisionBetween(13, 16);
-    map.setCollisionBetween(19, 22);
-    map.setCollisionBetween(25, 28);
-
-    // Colored Platforms
-    map.setCollisionBetween(36, 41);
-    map.setCollisionBetween(43, 46); //the ends are not collided with
-
-    // Slabs
-    map.setCollisionBetween(54,55); // flat slab
-    map.setCollisionBetween(56,57); map.setCollision(51); // positive slope slab
-    map.setCollisionBetween(58,59); map.setCollision(52); // negative slope slab
-
-    // Ceiling 
-    map.setCollisionBetween(61,64);
-    map.setCollision(66);
-
-    // Stone Blocks
-    map.setCollisionBetween(90, 94);
-    map.setCollisionBetween(96, 99);
-    map.setCollisionBetween(102, 103);
-    map.setCollisionBetween(108, 109);
-
-    // Water
-    // map.setCollision(78);
-    // map.setCollision(84);
-
-    // Heads
-    map.setCollisionBetween(80, 81);
-    map.setCollisionBetween(86, 87);
-
-    // Foliage
-    map.setCollision(83); map.setCollision(89); // long-bottom foliage
-    map.setCollisionBetween(110, 113); // 110-111 medium sized foliage, 112 - med-small, 113, smallest
-    map.setCollision(114); // ceiling foliage
-
-
-    //  Un-comment this on to see the collision tiles
     state.layer0.debug = true;
 
     return map;
 }
+
 
 function setCaveMapCollisionTiles(map) {
 	/* Set tile callbacks for special tiles */
